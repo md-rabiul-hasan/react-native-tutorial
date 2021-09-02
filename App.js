6bc8f1d69d3499500207a61d30035007f0607665
rabiulhasan3 <rabiul.fci@gmail.com>
2021-09-03 02:39:22 +0600

    view/text/style

--- App.js
+++ App.js
@@ -5,8 +5,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+          <Text style={styles.boldText} >Hello World !!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>Rabiul <Text style={styles.boldText} >Hasan</Text> </Text>
+        <Text>Lorem ipsum dolor sit amet.</Text>
+        <Text>Lorem ipsum dolor sit.</Text>
+      </View>
     </View>
   );
 }
@@ -18,4 +24,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    backgroundColor: 'pink',
+    padding: 20
+  },
+  boldText:{
+    fontWeight: 'bold'
+  },
+  body:{
+    backgroundColor: 'yellow',
+    padding: 20
+  }
 });
