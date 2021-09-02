12d0190c7a9f45a502c193dca4cfa641027e34b9
rabiulhasan3 <rabiul.fci@gmail.com>
2021-09-03 03:07:33 +0600

    textinput

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  
+  const [name, setName] = useState('jhon');
+  const [age, setAge]   = useState(1);
   return (
     <View style={styles.container}>
-        
+        <Text>My name is {name} and my age is {age} </Text>
+        <Text>Enter your name</Text>
+        <TextInput
+          style={styles.inputbox}
+          onChangeText={(val) => setName(val)}
+        />
+
+      <Text>Enter your age</Text>
+        <TextInput
+          keyboardType='numeric'
+          style={styles.inputbox}
+          onChangeText={(value) => setAge(value)}
+        />
+
     </View>
   );
 }
@@ -16,5 +30,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  inputbox : {
+    borderColor: 'red',
+    borderWidth: 1,
+    width: 200,
+    marginTop: 5,
+    marginBottom : 5
   }
 });
