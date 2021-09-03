3f77a6aa3e471f6826acc9253b961d96273cc4d4
rabiulhasan3 <rabiul.fci@gmail.com>
2021-09-03 10:08:40 +0600

    flat list

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [peoples, setPeoples] = useState([
+    {name:'Rabiul', id:1},
+    {name:'Hasan', id:2},
+    {name:'Kawsar', id:3},
+    {name:'Nasir', id:4},
+    {name:'Basir', id:5},
+    {name:'Rofiq', id:6},
+    {name:'Jabbar', id:7},
+    {name:'Mehedi', id:8},
+    {name:'Rubel', id:9},
+    {name:'Riyad', id:10}
+  ])
   
   return (
     <View style={styles.container}>
-        
+        <FlatList
+          data={peoples}
+          keyExtractor = { (item) => item.id }
+          renderItem = { ( {item} ) => (
+            <Text style={styles.item}>{item.name}</Text>
+          )}
+        />
     </View>
   );
 }
@@ -16,5 +35,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  item : {
+    flex:1,
+    marginHorizontal: 10,
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24
   }
 });
